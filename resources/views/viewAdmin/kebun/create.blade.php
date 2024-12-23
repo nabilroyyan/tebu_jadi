0ea149f1ac5134de8568597fcead049300e5594f
vrayoga <vrayogal227@gmail.com>
2024-12-23 08:16:27 +0700

    perbaiki data

--- resources/views/viewAdmin/kebun/create.blade.php
+++ resources/views/viewAdmin/kebun/create.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     
 
-<form action="/kebun-create" method="POST">
+<form action="/kebun-store" method="POST">
     @csrf
     <div class="row">
         <div class="col-lg-6">
